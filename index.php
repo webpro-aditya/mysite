8a28fa76a10e4b6d4fe811fc8fdc263389cef0d4
aditya <webpro.aditya@gmail.com>
2021-08-26 23:37:14 +0530

    Mysite

---
+++ index.php
@@ -0,0 +1,396 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="css/responsive.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=PT+Mono&family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+    <script src="https://kit.fontawesome.com/30ba718937.js" crossorigin="anonymous"></script>
+
+    <title>My Personal Website</title>
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <nav>
+                <div class="toggle_button">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <a href="javaScript:void(0)">
+                    <span class="logo_text">
+                        AD
+                    </span>
+                </a>
+                <ul>
+                    <li>
+                        <a href="#main_content">
+                            Home
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#projects">
+                            Projects
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#blogs">
+                            Blogs
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#expertise">
+                            Expertise
+                        </a>
+                    </li>
+                     <li>
+                        <a href="#contact">
+                            Contact
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <section id="main_content">
+        <div class="container">
+            <h1>
+                <span>Hi,</span><br />
+                <span>I'm Aditya,</span><br />
+                <span>Web Developer</span><br />
+                <span>Frontend Developer / Backend Developer</span>
+            </h1>
+            <button><span id="button_text">Contact Me!</span><span id="hover_effect"></span></button>
+
+        </div>
+    </section>
+
+    <section id="expertise">
+        <div class="container">
+            <h2>My Expertise in Technologies</h2>
+            <div class="row">
+                <div class="col">
+                    <i class="fas fa-laptop-code"></i>
+                    <h3>Front-end Developer</h3>
+                    <p>I like to code things from scratch, and enjoy bringing ideas to life in the browser with
+                        available tools.</p>
+                    <h4>Languages I speak:</h4>
+                    <span>HTML, CSS, JavaScript, JQuery</span>
+                    <h4>Dev Tools / Technologies</h4>
+                    <ul>
+                        <li>Bootstrap</li>
+                        <li>ReactJS</li>
+                        <li>Angular</li>
+                        <li>Github</li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <i class="fas fa-code"></i>
+                    <h3>Back-end Developer</h3>
+                    <p>I write clean code with developing applications which are not only user-friendly but serve the
+                        mankind.</p>
+                    <h4>Technologies I master:</h4>
+                    <span>PHP, NodeJS</span>
+                    <h4>Dev Tools / Technologies</h4>
+                    <ul>
+                        <li>MySql</li>
+                        <li>PostgreSql</li>
+                        <li>Laravel</li>
+                        <li>Wordpress</li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <i class="fas fa-users"></i>
+                    <h3>Mentor</h3>
+                    <p>I genuinely care about people, and love helping fellow designers work on their craft.</p>
+                    <h4>Experiences I draw from:</h4>
+                    <span>Frontend and Backend Dev Training</span>
+                    <h4>Mentor Stats:</h4>
+                    <ul>
+                        <li>5 years experience</li>
+                        <li>26 short courses</li>
+                        <li>42 bootcamps</li>
+                        <li>125+ students</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="projects">
+        <div class="container">
+            <h2>My Projects</h2>
+            <p>Coming Soon!</p>
+        </div>
+    </section>
+
+
+    <section id="blogs">
+        <div class="container">
+            <h2>My Blogs</h2>
+            <div class="row">
+                <div class="post">
+                    <a href="https://techatma.in/web-hosting/"><img
+                            src="https://techatma.in/wp-content/uploads/2020/05/what-is-web-hosting.jpg"></a>
+                    <h3><a href="https://techatma.in/web-hosting/">Linux Web Hosting and Different Types</a></h3>
+                    <p>Web hosting is the vendor that allows an individual or an agency in getting their internet site
+                        accessible on the World Wide Web. Hosting is an..</p>
+                    <a href="https://techatma.in/web-hosting/"><span class="r1">Read More..</span><span
+                            class="r2"></span></a>
+                </div>
+                <div class="post">
+                    <a href="https://techatma.in/5-top-industries-to-be-transformed-by-artificial-intelligence-soon/"><img
+                            src="https://techatma.in/wp-content/uploads/2019/06/artificial-intelligence-techatma.jpg"></a>
+                    <h3><a
+                            href="https://techatma.in/5-top-industries-to-be-transformed-by-artificial-intelligence-soon/">Industries
+                            transformed by Artificial Intelligence</a></h3>
+                    <p>Artificial Intelligence is a range of computer science related to developing smart machines that
+                        are capable of replicating human intelligence.</p>
+                    <a href="https://techatma.in/5-top-industries-to-be-transformed-by-artificial-intelligence-soon/"><span
+                            class="r1">Read More..</span><span class="r2"></span></a>
+                </div>
+                <div class="post">
+                    <a href="https://techatma.in/is-windows-defender-better-than-avast/"><img
+                            src="https://techatma.in/wp-content/uploads/2020/04/windows-defender-avast-techatma-india-scaled.jpg"></a>
+                    <h3><a href="https://techatma.in/is-windows-defender-better-than-avast/">Comparison Windows Defender
+                            and Avast</a></h3>
+                    <p>Windows Defender Antivirus or popularly known by the name Windows Defender is a form of
+                        anti-malware application that is part of the..</p>
+                    <a href="https://techatma.in/is-windows-defender-better-than-avast/"><span class="r1">Read
+                            More..</span><span class="r2"></span></a>
+                </div>
+                <div class="post">
+                    <a href="https://techatma.in/photo-recovery-software-html/"><img
+                            src="https://techatma.in/wp-content/uploads/2018/08/Photo-Recovery-Software-Techatma.jpg"></a>
+                    <h3><a href="https://techatma.in/photo-recovery-software-html/">Recover Deleted Images from Memory
+                            Card</a></h3>
+                    <p>Photos/images are surely a significant part of one’s life. From capturing the fun-filled moments
+                        to taking a shot of an official document, images.. </p>
+                    <a href="https://techatma.in/photo-recovery-software-html/"><span class="r1">Read More..</span><span
+                            class="r2"></span></a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+        <section id="contact">
+            <div class="left">
+                <h2>Contact Me</h2>
+                <p>I am interested in Full time job opportunities and Freelance projects. If you have any kind of question,
+                    don't hesitate to use the form.</p>
+                <form action="index.php" method="post">
+                    <div class="namenemail">
+                        <input type="text" name="name" placeholder="Name" autocomplete="off" required>
+                        <input type="email" name="email" placeholder="Email" autocomplete="off" required>
+                    </div>
+                    <input type="text" name="subject" placeholder="Subject" autocomplete="off">
+                    <textarea name="message" placeholder="Message"></textarea>
+                    <button type="submit" name="submit"><span id="textbox">Send message!</span><span
+                            id="hoverbox"></span></button>
+                </form>
+                <?php
+                  if(isset($_POST['submit'])){
+                    $con = mysqli_connect('localhost', 'root', '', 'mysite');
+
+                    $name = $_POST['name'];
+                    $email = $_POST['email'];
+                    $subject = $_POST['subject'];
+                    $message = $_POST['message'];
+
+                    if($name=='' || $email=='' || $subject=='' || $message==''){
+                        echo "<div id='error_box'>Please Fill all the Fields!</div>";
+                        
+                    }
+
+                    $ins = "INSERT INTO `messages` (name, email, subject, message) VALUES ('$name', '$email', '$subject', '$message')";
+                    $myquery = mysqli_query($con, $ins);
+
+                    if($myquery){
+                        echo "<div id='message_box'>Thanks for Contacting!</div>";
+                    }
+                    else{
+                       "<div id='error_box'>Error in Submitting!</div>"; 
+                       header("Location: index.php#contact", TRUE, 301);
+                    }
+
+                  }
+                ?>
+            </div>
+            <div class="right">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d7419435.663033565!2d77.80830159916987!3d24.769028641882127!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1626606025233!5m2!1sen!2sin"
+                    style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            </div>
+        </section>
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <ul>
+                        <li>
+                            <a href="index.php">
+                                Home
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#projects">
+                                Projects
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#blogs">
+                                Blogs
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#expertise">
+                                Expertise
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                </div>
+            </div>
+           <!--  <div class="row">
+                <span>Copyright &copy;2021 AD. All rights reserved.</span>
+            </div> -->
+        </div>
+    </footer>
+
+    <script src="js/jquery-3.6.0.min.js"></script>
+    <script>
+        $(document).ready(function () {
+            let a = 1;
+            $('.toggle_button').click(function () {
+                $('nav ul').slideToggle(800);
+
+                let div1 = $('.toggle_button div:first-child');
+                let div2 = $('.toggle_button div:nth-child(2)');
+                let div3 = $('.toggle_button div:last-child');
+
+                if(a == 1){
+
+                div1.css({
+                    'transform': 'rotate(45deg) translate(5px, 10px)',
+                });
+                div2.css({
+                   
+                    'opacity': '0'
+                });
+                div3.css({
+                    'transform': 'rotate(-45deg) translate(5px, -10px)',
+                });
+                return a = 0;
+                }
+                else{
+                  div1.css({
+                    'transform': 'rotate(0)',
+                });
+                div2.css({
+                    
+                    'opacity': '1'
+                });
+                div3.css({
+                    'transform': 'rotate(0)',
+                });
+                return a = 1;
+                }
+            });
+        });
+    </script>
+
+    <script src="js/jquery.scrollspy.js"></script>
+    <script>
+        $('nav, footer').scrollSpy({
+            scrollDuration: 1000,
+            scrollEasing: 'swing',
+        });
+    </script>
+
+    <script>
+        $(window).scroll(function () {
+            let pos = $(window).scrollTop();
+
+            if (pos > 100) {
+                $('header').addClass('fixed-nav');
+                // $('nav ul').css({
+                //    'transform': 'translateY(-30px)'
+                // });
+            }
+            else {
+                $('header').removeClass('fixed-nav');
+                // $('nav ul').css({
+                //    'transform': 'translateY(0)'
+                // })
+            }
+        })
+    </script>
+
+    <script src="js/jquery.scrollify.js"></script>
+    <script>
+        $.scrollify({
+    section : "section",
+    sectionName : "section-name",
+    interstitialSection : "",
+    easing: "easeOutExpo",
+    scrollSpeed: 1100,
+    offset : -210,
+    scrollbars: true,
+    standardScrollElements: "#contact, footer",
+    setHeights: false,
+    overflowScroll: true,
+    updateHash: false,
+    touchScroll:true
+  });
+
+    </script>
+    <script src="js/jquery.scrollUp.min.js"></script>
+    <script>
+        $(function () {
+    $.scrollUp({
+        scrollName: 'scrollUp',      // Element ID
+        scrollDistance: 300,         // Distance from top/bottom before showing element (px)
+        scrollFrom: 'top',           // 'top' or 'bottom'
+        scrollSpeed: 300,            // Speed back to top (ms)
+        easingType: 'linear',        // Scroll to top easing (see http://easings.net/)
+        animation: 'fade',           // Fade, slide, none
+        animationSpeed: 200,         // Animation speed (ms)
+        scrollTrigger: false,        // Set a custom triggering element. Can be an HTML string or jQuery object
+        scrollTarget: false,         // Set a custom target element for scrolling to. Can be element or number
+        scrollText: '', // Text for element, can contain HTML
+        scrollTitle: false,          // Set a custom <a> title if required.
+        scrollImg: false,            // Set true to use image
+        activeOverlay: false,        // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+        zIndex: 2147483647           // Z-Index for the overlay
+    });
+});
+
+    </script>
+
+<script>
+$(document).ready(function(){
+    $('#message_box').fadeOut(10000);
+
+    $('#error_box').fadeOut(10000);
+});  
+</script>
+
+
+</body>
+
+</html>
